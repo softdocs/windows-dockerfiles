e939162ed12f2d7fa627ba3078adc06a4a730fa3
spresti <spresti@softdocs.com>
2020-03-05 12:40:41 -0500

    Added more durrable logic for the starup process. As some computers are slower. Still using this old way of changing users due to rabbitmq wait not working in 3.6.15, I expect this logic to slim down and be more purposeful useing more recient default settings.

--- rabbit-mq/firstStart.ps1
+++ rabbit-mq/firstStart.ps1
@@ -1,10 +1,16 @@
 $hasRunPath = "C:/setupDone"
-Start-Sleep -Seconds 40
+$i=0
+do{
+    Start-Sleep -Seconds 20
+    if ( -not (Test-Path $hasRunPath)) {
+        Write-Host "Setting up ADMIN and BASE Users"
+        try{
+            ./rabbitmqctl.bat add_user $env:BASE_USERNAME $env:BASE_PASSWORD  
+            ./rabbitmqctl.bat change_password admin $env:ADMIN_PASSWORD
+            ./rabbitmqctl.bat set_permissions -p / $env:BASE_USERNAME .* .* .*
+        }catch{}
+    }
+    $i++
+}while($i -lt 10)
 
-if ( -not (Test-Path $hasRunPath)) {
-    Write-Host "Setting up ADMIN and BASE Users"
-    ./rabbitmqctl.bat add_user $env:BASE_USERNAME $env:BASE_PASSWORD  
-    ./rabbitmqctl.bat change_password admin $env:ADMIN_PASSWORD
-    ./rabbitmqctl.bat set_permissions -p / $env:BASE_USERNAME .* .* .*
-    Write-Output $null >> $hasRunPath;
-}
+Write-Output $null >> $hasRunPath;
